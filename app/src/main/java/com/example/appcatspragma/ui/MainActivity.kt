5b02df58c3584ef45ae4386041ba756821e51ced
sandres0190 <sandres0190@gmail.com>
2023-04-18 09:03:48 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/appcatspragma/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appcatspragma.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appcatspragma.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
